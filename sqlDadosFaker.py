da86d6fd3edd57b60719e809afb715a50b266e0a
joca2511 <143613939+joca2511@users.noreply.github.com>
2024-05-06 19:04:11 -0300

    Atualizando sqlDadosFaker
    Terminando a criação de IDs

--- sqlDadosFaker.py
+++ sqlDadosFaker.py
@@ -7,10 +7,29 @@ import random
 ## dept_id possui 4 digitos
 ## disc_id possui 3 digitos
 
-# arquivo = open("dadosSQL","w") ##arquivo para depois usar no sql
+
 
 faker = Faker("pt_BR") ##nomes brasileiros
+
 qtnomes = 30 ## 30 nomes
+qtalunos = 20 ## qtde de alunos 
+qtprof = qtnomes-qtalunos ## qtde de prof
+qtcurso = 6 ##qt de cursos
+qttcc = 3 ##qt de tccs
+qtdept = 3 ##qt de dept
+qtdisc = 3 ##qt de disc
+
+def montarID (qtIds,qtDigitos):
+    Array = []
+    digitomax = ""
+    for x in range(qtDigitos):
+        digitomax +="9"
+    for x in range(qtIds):
+        novoid = random.randint((10**(qtDigitos-1)),int(digitomax))
+        while novoid in Array:
+            novoid = random.randint((10**(qtDigitos-1)),int(digitomax))
+        Array.append(novoid)
+    return Array
 
 nomes = [] ##nomes de alunos e profs
 for x in range(qtnomes):
@@ -21,21 +40,79 @@ for x in range(qtnomes):
     novonome += " "
     novonome += novo2onome
     nomes.append(novonome)
-
+    
+print("Nomes:")
 print(nomes)
-qtalunos = 20 ## qtde de alunos 
-qtprof = qtnomes-qtalunos ## qtde de prof
-aluno_id = [] ##ids dos alunos
 
-for x in range(qtalunos): ##criando id dos alunos (8 digitos)
-    novoid = random.randint(10000000,99999999) ##atualmente eh completamente aleatorio
 
-    if novoid in aluno_id: ##para nao ter id igual
-        novoid = random.randint(10000000,99999999)
 
-    aluno_id.append(novoid)
 
+aluno_id = montarID(qtalunos,8)
+print("ID Alunos: ")
 print(aluno_id)
+
+
+
+
+
+prof_id = montarID(qtprof,7)
+print("ID prof: ")
+print(prof_id)
+
+
+curso_nomes = [] ##adicionar na mao (ter certeza que ha mais nomes que qt!)
+curso_id = montarID(qtcurso,6)   
+
+print("ID curso: ")
+print(curso_id)
+
+
+
+tcc_nomes = [] ##adicionar na mao (ter certeza que ha mais nomes que qt!)
+tcc_id = montarID(qttcc,5)
+print("ID tcc: ")
+print(tcc_id)
+
+
+dept_nomes = [] ##adicionar na mao (ter certeza que ha mais nomes que qt!)
+dept_id = montarID(qtdept,4)   
+print("ID dept: ")
+print(dept_id)
+
+
+disc_nomes = [] ##adicionar na mao (ter certeza que ha mais nomes que qt!)
+disc_id = montarID(qtdisc,3)
+print("ID Disc: ")
+print(disc_id)
+
+
+##Comecar os inserts (colocar coisas no arquivo)
+
+arquivo = open("dadosSQL","w",encoding="utf-8") ##arquivo para depois usar no sql
+
+for x in range(qtalunos): ##inserir dados aluno
+    arquivo.write("insert into aluno (%s,%s,curso_id);\n" %(aluno_id[x],nomes[x])) ##falta terminar logica
+    arquivo.write("insert into historico_aluno (%s,disc_id,nota,semestre,ano);\n" %(aluno_id[x]))
+
+for x in range(qtcurso): ##inserir dados curso
+    arquivo.write("insert into curso (%s,nome,dept_id);\n" %(curso_id[x])) ##falta terminar logica
+
+for x in range(qtdept): ##inserir dados dept
+    arquivo.write("insert into departamento (%s,nome,chefe_id);\n" %(dept_id[x])) ##falta terminar logica
+    
+for x in range(qtdisc): ##inserir dados disc
+    arquivo.write("insert into disciplina (%s,nome,dept_id);\n" %(disc_id[x])) ##falta terminar logica
+    
+integrantestcc = 2 ##qt de integrantes por tcc
+for x in range(qttcc): ##inserir dados tcc
+    arquivo.write("insert into tcc(%s,nome,prof_id,curso_id,ano,semestre);\n" %(tcc_id[x])) ##falta terminar logica
+    for y in range(integrantestcc):
+        arquivo.write("insert into part_tcc(%s,aluno_id);\n"%(tcc_id[x])) ##falta terminar logica
+
+for x in range(qtprof): ##inserir dados prof
+    arquivo.write("insert into professor (%s,%s);\n" % (prof_id[x],nomes[qtalunos+x])) ##falta terminar logica
+    arquivo.write("insert into historico_professor(%s,disc_id,semestre,ano);\n" %(prof_id[x])) ##falta terminar logica
+
 
 
 
